23430c697eedd92e8bf48c7d5d35fd37bbfe0edb
Girish21 <girishkumarv21@gmail.com>
2023-08-09 00:23:40 +0530

    init project setup 🔥

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import path from "path";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(morgan("dev"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
